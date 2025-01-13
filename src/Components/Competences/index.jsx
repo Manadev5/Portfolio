99f1644565afb876721f6c90fc21c51bebd0b1f5
Manassè MUKENGE <manakabamba@gmail.com>
2025-01-13 21:06:26 +0100

    section compétences debuted

--- src/Components/Competences/index.jsx
+++ src/Components/Competences/index.jsx
@@ -7,48 +7,51 @@ function competences() {
 
 return(
     
-    <section id ='section-competences' >
+  <section id ='section-competences' >
 
-   <h2 className='titre-3'> Mes compétences</h2>
+    <h2 className='titre-3'> Mes compétences</h2>
 
-   <div id="Competences">
+    <div id="Competences">
 
       <div className='block-1'>
         <img src= {frontend} alt='frontend-logo'/>
         <ul>
-           <li>HTML5</li>
-           <li>CSS3/SASS/SCSS</li>
-           <li>JavaScript</li>
-          <li>React.js</li>
+          <li>HTML5</li>
+          <li>CSS3/SASS/SCSS</li>
+          <li>JavaScript</li>
+          <li>ReactJs</li>
           <li>Angular</li>
-       </ul>
-     </div>
+        </ul>
+      </div>
 
       <div className='block-2'>
         <img src= {backend} alt='backend-logo'/>
         <ul>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>PHP</li>
-          <li>Symfony</li>
-       </ul>
+        <li>PHP</li>
+        <li>Symfony</li>
+        <li>C#</li>
+        <li>.Net</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        </ul>
       </div>
 
-     <div className='block-3'>
-      <img src= {outils} alt='outils-logo'/>
-      <ul> 
-        <li>Mysql</li>
-        <li>MongoDb</li>
-        <li>Visual Studio Code</li>
-        <li>Terminal</li>
-        <li>Docker</li>
-        <li>Figma</li>
-      </ul>
+      <div className='block-3'>
+        <img src= {outils} alt='outils-logo'/>
+        <ul> 
+          <li>Mysql</li>
+          <li>MongoDb</li>
+          <li>Visual Studio Code</li>
+          <li>Visual Studio</li>
+          <li>Github</li>
+          <li>Docker</li>
+          <li>Figma</li>
+        </ul>
       </div>
 
     </div>
 
-    </section>
+  </section>
 
 )
 
