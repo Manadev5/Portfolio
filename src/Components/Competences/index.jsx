2af5ce38f83784c0702df7019448a1784d09df87
Manassè MUKENGE <manakabamba@gmail.com>
2025-01-14 21:05:42 +0100

    Competences finished

--- src/Components/Competences/index.jsx
+++ src/Components/Competences/index.jsx
@@ -15,17 +15,21 @@ return(
 
       <div className='block-1'>
         <img src= {frontend} alt='frontend-logo'/>
+        <h3>Frontend</h3>
         <ul>
-          <li>HTML5</li>
-          <li>CSS3/SASS/SCSS</li>
           <li>JavaScript</li>
           <li>ReactJs</li>
           <li>Angular</li>
+          <li>Bootstrap</li>
+          <li>tailwind</li>
+          <li>HTML5</li>
+          <li>CSS3/SASS/SCSS</li>
         </ul>
       </div>
 
       <div className='block-2'>
         <img src= {backend} alt='backend-logo'/>
+        <h3>Backend</h3>
         <ul>
         <li>PHP</li>
         <li>Symfony</li>
@@ -33,17 +37,19 @@ return(
         <li>.Net</li>
         <li>Node.js</li>
         <li>Express.js</li>
+        <li>SQL / noSQL</li>
         </ul>
       </div>
 
       <div className='block-3'>
         <img src= {outils} alt='outils-logo'/>
-        <ul> 
-          <li>Mysql</li>
-          <li>MongoDb</li>
+        <h3>Outils</h3>
+        <ul>
           <li>Visual Studio Code</li>
           <li>Visual Studio</li>
           <li>Github</li>
+          <li>Bitbucket</li>
+          <li>Jira</li>
           <li>Docker</li>
           <li>Figma</li>
         </ul>
