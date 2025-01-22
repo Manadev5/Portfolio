1fdf23a172dfea8c40a091ff490db408b9da1b50
Manassè MUKENGE <manakabamba@gmail.com>
2025-01-22 22:58:34 +0100

    project part logic works

--- src/Components/Projets/index.jsx
+++ src/Components/Projets/index.jsx
@@ -1,77 +1,40 @@
 import { useState } from "react";
-import Right from '../../Assets/Vector-right.png'
-import Left from '../../Assets/Vector-left.png'
-function Projets({Datas }){
+import Right from '../../Assets/Vector-right.png';
+import Left from '../../Assets/Vector-left.png';
 
-     const [showdetails, setShowdetails] = useState(false);
-   
-  //   const goForward = (i) =>{
+const ListItem = ({ data }) => {
+  const [showDetails, setShowDetails] = useState(false);
 
-  //    const lastSlide = currentIndex === Datas.length - 1;
-  //    const newIndex= lastSlide ? 0 : currentIndex + 1;
-  //    setCurrentIndex(newIndex)
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
 
-  //   };
-
-  //   const goBackward = (i) => {
-  //       const firstSlide = currentIndex === 0;
-  //       const newIndex= firstSlide ? Datas.length - 1 : currentIndex -1;
-  //       setCurrentIndex(newIndex)
-
-        
-  //   }
-
-  //  index = currentIndex;
-
-
-
-return(
-
-      
-  <div>  
-  <ul className="projects-list">{  Datas.map((data)=>(
+  return (
     <li className="full-item">
-      <div>
+      <div onClick={toggleDetails}>
         <h3 className="item-title">{data.title}</h3>
-        
       </div>
-      <div className={showdetails ? 'details': 'details-none'}>
-        <img src={data.cover} alt ={data.title}/>
-        <p>{data.description}</p>
-      </div>
+      {showDetails && (
+        <div className="details">
+          <img src={data.cover} alt={data.title} />
+          <p>{data.description}</p>
+        </div>
+      )}
       <div className="line-blue"></div>
     </li>
-  ))}</ul>
+  );
+};
 
-  {/* carrousel  */} 
-  {/* <div key ={Datas.id} className="image-container" >
-      <ul>
+function Projets({Datas}){   
+  
+  return (
+    <div>
+      <ul className="projects-list">
+        {Datas.map((data, index) => (
+          <ListItem key={index} data={data} />
+        ))}
       </ul>
-      <div className="slider">
-        <img src = {Left} alt='vector-left' onClick= {()  => (goBackward(index))}  className='vector-left'/>
-        
-          <img src = {Datas[index].cover} alt={Datas[index].title} className='image-slider'/>
-
-          <img src = {Right} alt='vector-right' onClick= {()  => (goForward(index))} className='vector-right'/>
-         
-      </div>
-          
-        
-      <div className="content">
-        <h3>{Datas[index].title}</h3>
-        <h5 className="description">{Datas[index].description}</h5>
-
-        <div className="link">
-          <a href ={Datas[index].link}>Voir le code sur github</a>
-        </div>
-
-      </div>
-
-       
-     
-
-    </div> */}
-    </div> 
+    </div>
 
 )
 
